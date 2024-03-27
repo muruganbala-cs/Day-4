464b9a6b9a566c4a681abd03fe4908ac16c91e67
muruganbala-cs <balamaths.pmk@gamil.com>
2024-03-27 11:13:15 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,26 @@
+let resume={
+    "name":"bala",
+    "DOB":"29.09",
+    "qualification":"BSc",
+    "Skills":"Fullstockdevelope",
+    "language":"english",
+    "nationality":"indian",
+
+}
+console.log(resume.name)
+console.log(resume.DOB)
+console.log(resume.qualification)
+console.log(resume.Skills)
+console.log(resume.language)
+console.log(resume.nationality)
+// for in loop
+for (key in resume){
+    console.log(resume[key])
+}
+////for of loop
+let data=Object.entries(resume)
+for (let [key,value] of data){
+console.log(value);
+}
+// for each
+data.forEach(([key,val])=>console.log(val))
